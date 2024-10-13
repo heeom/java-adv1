39f8efc2b6aade16ec095ad720cc3af4274dca96
heeom <swimxdev@gmail.com>
2024-10-13 22:02:21 +0900

    first commit feat : join 사용

--- src/main/java/com/example/thread/control/join/JoinMainV1.java
+++ src/main/java/com/example/thread/control/join/JoinMainV1.java
@@ -5,7 +5,7 @@ import static com.example.util.ThreadUtils.sleep;
 
 public class JoinMainV1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         log("start");
         SumTask task1 = new SumTask(1, 50);
         SumTask task2 = new SumTask(51, 100);
@@ -13,6 +13,12 @@ public class JoinMainV1 {
         Thread thread2 = new Thread(task2, "thread-2");
         thread1.start();
         thread2.start();
+
+        // 스레드가 종료될때까지 대기
+        log("main 스레드가 thread1,2 종료(TERMINATED)까지 대기");
+        thread1.join(); // main 스레드 : WAITING상태
+        thread2.join();
+        log("대기 완료");
         log("task1 = " + task1.result);
         log("task2 = " + task2.result);
 
