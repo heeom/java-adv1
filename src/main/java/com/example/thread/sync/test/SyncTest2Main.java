22337e2cf008446563d26fea6f7075140f78e159
heeom <swimxdev@gmail.com>
2024-10-21 20:29:47 +0900

    feat : test2

---
+++ src/main/java/com/example/thread/sync/test/SyncTest2Main.java
@@ -0,0 +1,30 @@
+package com.example.thread.sync.test;
+
+import static com.example.util.MyLogger.log;
+
+public class SyncTest2Main {
+
+    public static void main(String[] args) {
+        MyCounter myCounter = new MyCounter();
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                myCounter.count();
+            }
+        };
+        Thread thread1 = new Thread(task, "Thread-1");
+        Thread thread2 = new Thread(task, "Thread-2");
+        thread1.start();
+        thread2.start();
+    }
+
+    static class MyCounter {
+        public void count() {
+            int localValue = 0; // 지역변수는 다른 스레드와 공유하지 않는데, 스레드의 개별 공간인 스택영역에 생성된다
+            for (int i = 0; i < 1000; i++) {
+                localValue = localValue + 1;
+            }
+            log("결과: " + localValue);
+        }
+    }
+}
