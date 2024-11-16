51e15b23b3405a8aec9371e166108ebfff59bb1e
heeom <swimxdev@gmail.com>
2024-11-16 17:31:27 +0900

    feat : AtomicInteger 주석 추가

--- src/main/java/com/example/thread/cas/increment/MyAtomicInteger.java
+++ src/main/java/com/example/thread/cas/increment/MyAtomicInteger.java
@@ -3,6 +3,10 @@ package com.example.thread.cas.increment;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * 특정 값을 증가하거나 감소해야하는데, 여러 스레드가 해당 값을 공유해야한다면, AtomicInteger을 사용하면 된다
+ * AtomicInteger, AtomicLong, AtomicBoolean 등 다양한 AtomicXXX 클래스가 존재한다.
+ */
 public class MyAtomicInteger implements IncrementInteger {
 
     AtomicInteger atomicInteger = new AtomicInteger(0);
