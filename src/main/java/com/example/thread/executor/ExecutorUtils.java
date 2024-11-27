4c4608c1b48126a8089d6b0cfb068eef59d13f6f
heeom <swimxdev@gmail.com>
2024-11-27 21:56:30 +0900

    feat : ExecutorUtils

---
+++ src/main/java/com/example/thread/executor/ExecutorUtils.java
@@ -0,0 +1,21 @@
+package com.example.thread.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static com.example.util.MyLogger.log;
+
+public abstract class ExecutorUtils {
+
+    public static void printState(ExecutorService executorService) {
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            int poolSize = poolExecutor.getPoolSize();
+            int activeCount = poolExecutor.getActiveCount();
+            int queue = poolExecutor.getQueue().size();
+            long completedTaskCount = poolExecutor.getCompletedTaskCount();
+            log("[pool=" + poolSize + ", active = " + activeCount + ", queueTasks = " + queue + ", completedTask = " + completedTaskCount);
+        } else {
+            log(executorService);
+        }
+    }
+}
