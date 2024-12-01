3fd800681faf8a376e463542949bec10dfa960e5
heeom <swimxdev@gmail.com>
2024-12-01 15:16:57 +0900

    feat : future동작 방식 주석 추가

--- src/main/java/com/example/thread/executor/future/CallableMainV1.java
+++ src/main/java/com/example/thread/executor/future/CallableMainV1.java
@@ -11,8 +11,8 @@ public class CallableMainV1 {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService executorService = Executors.newFixedThreadPool(1);
-        Future<Integer> future = executorService.submit(new MyCallable());
-        Integer result = future.get();
+        Future<Integer> future = executorService.submit(new MyCallable()); // Callable 인스턴스가 블로킹 큐에 전달됨 -> 스레드풀의 스레드 중 하나가 작업 실행 -> 작업 결과 반환
+        Integer result = future.get(); // 작업이 완료가 안됐다면? -> 결과를 나중에 받을 수 있는 Future 객체를 대신 제공한다.
         log("result value = " + result);
         executorService.close();
     }
--- src/main/java/com/example/thread/executor/future/CallableMainV2.java
+++ src/main/java/com/example/thread/executor/future/CallableMainV2.java
@@ -10,18 +10,21 @@ public class CallableMainV2 {
 
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        ExecutorService executorService = Executors.newFixedThreadPool(1); // 스레드 풀 생성
         log("submit() 호출");
-        Future<Integer> future = executorService.submit(new MyCallable());
-        log("future 즉시 반환, future = " + future);
+        Future<Integer> future = executorService.submit(new MyCallable()); // Future 인터페이스의 구현체 -> FutureTask 생성
+        // 생성한 Future 객체에 MyCallable 인스턴스 보관 -> 인스턴스 내부에 task 작업완료여부, 작업의 결과물 보관
+        // submit 호출시 task가 바로 블로킹 큐에 담기는 것이 아니라 task를 감싸고 있는 Future가 대신 BlockingQueue에 담긴다.
+        log("future 즉시 반환, future = " + future); // 작업 전달시 생성한 Future는 즉시 반환됨, 요청(main) 스레드는 대기하지 않음
+
         log("future.get() [블로킹] 메서드 호출 시작 -> main 스레드 WAITING");
+        // future 실행 -> futureTask.run() -> myCallable.call() 호출
+        Integer result = future.get(); // 블로킹 : Future가 완료상태가 될때까지 대기
+        log("future.get() [블로킹] 메서드 호출 완료 -> main 스레드 RUNNABLE");
 
-        Integer result = future.get();
 
-        log("future.get() [블로킹] 메서드 호출 시작 -> main 스레드 RUNNABLE");
-
-        log("result value = " + result);
-        log("future 완료, future = " + future);
+        log("result value = " + result); // task 작업 완료 -> future에 작업 결과 담고, future 완료 상태로 변경
+        log("future 완료, future = " + future); // main 스레드 : WAITING -> RUNNABLE 상태가 됨
         executorService.close();
     }
     
